ef7f6090f3e0b7721b1306e454784b4b3360e044
ngyv <ngyv.codes@gmail.com>
2018-04-02 18:52:00 +0800

    [UTIL] Fetcher

---
+++ test.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import generateApi from './lib/fetcher';
+
+test('Fetcher', t => {
+  window.crsfToken = 'token';
+  const api = generateApi('/api', 'user');
+  t.plan(4);
+  t.is(typeof api.get, 'function', 'Has "get" function');
+  t.is(typeof api.post, 'function', 'Has "post" function');
+  t.is(typeof api.put, 'function', 'Has "put" function');
+  t.is(typeof api.delete, 'function', 'Has "delete" function');
+});
