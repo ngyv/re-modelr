f4f430a45e14936f5cb828e42755472e0dc0a1f3
ngyv <ngyv.codes@gmail.com>
2018-04-30 20:54:36 +0800

    Clean up currently unused variables and remove semicolon

--- src/domain-store.js
+++ src/domain-store.js
@@ -1,9 +1,8 @@
-import { generateApi } from './fetcher';
-import { types, identify, isEqual, comparePropertyToType } from '@ngyv/prop-utils';
-import objectUtils from '@ngyv/object-utils';
-const { setObject, pluckSubset, pushObject, mergeObject } = objectUtils;
+import { generateApi } from './fetcher'
+import { types, comparePropertyToType } from '@ngyv/prop-utils'
+import objectUtils from '@ngyv/object-utils'
+const { setObject, mergeObject } = objectUtils
 
-import inflection from 'inflection'
 import snakecase from 'lodash.snakecase'
 
 /*  Base store wrapper that handles fetching data from the server.
@@ -28,7 +27,6 @@ import snakecase from 'lodash.snakecase'
  *   @param { basePath='string' }
  *   [NOT CONFIRMED]   @param { children=[] }
  */
-const whitelisted = [ 'status' ]
 
 export default class DomainStore {
   _api = {}
@@ -38,56 +36,56 @@ export default class DomainStore {
   constructor(ModelClass, options = { basePath: '/api', modelName: '' }) {
     this.ModelClass = ModelClass
     this.modelName = options.modelName || this.ModelClass.name.toLowerCase()
-    this._generateApi(options.basePath, this.modelName);
+    this._generateApi(options.basePath, this.modelName)
     // this._processModelRelationships()
   }
 
   // returns
   _generateApi(basePath, modelName) {
-    this._api = generateApi(basePath, modelName);
+    this._api = generateApi(basePath, modelName)
   }
 
   // Only creates a model object but doesn't push to store
   _createRecord(modelJson, modelStatus) {
-    let record = new this.ModelClass(this, modelJson, modelStatus);
-    return record;
+    let record = new this.ModelClass(this, modelJson, modelStatus)
+    return record
   }
 
   // Gets an array of json and convert it into a hash of models with id as key
   _normalizeModels(models) {
     return models.reduce((modelsHash, modelJson) => {
-      modelsHash[modelJson.id] = this._createRecord(modelJson);
-      return modelsHash;
-    }, { length: models.length });
+      modelsHash[modelJson.id] = this._createRecord(modelJson)
+      return modelsHash
+    }, { length: models.length })
   }
 
   _pushEntry(modelJson) {
-    this.entries[modelJson.id] = this._createRecord(modelJson);
-    delete this.entries.length;
-    this.entries.length = Object.keys(this.entries).length;
-    return this.entries[modelJson.id];
+    this.entries[modelJson.id] = this._createRecord(modelJson)
+    delete this.entries.length
+    this.entries.length = Object.keys(this.entries).length
+    return this.entries[modelJson.id]
   }
 
   _deleteEntry(id) {
-    delete this.entries[id];
-    delete this.entries.length;
-    this.entries.length = Object.keys(this.entries).length;
+    delete this.entries[id]
+    delete this.entries.length
+    this.entries.length = Object.keys(this.entries).length
   }
 
   _genericError(error) {
-    console.error(error);
+    console.error(error) // eslint-disable-line no-console
   }
 
   _fetchAll(params = {}) {
-    return this._api.get(params);
+    return this._api.get(params)
   }
 
   _fetchAllSuccess(data) {
     // TODO: show in documents how to pre-process the data like below
     //let data = response.data[inflection.pluralize(this.modelName)]
-    this.entries = this._normalizeModels(data);
+    this.entries = this._normalizeModels(data)
 
-    // let entriesArray = Object.values(this.entries);
+    // let entriesArray = Object.values(this.entries)
     // if (this._relationships.length && entriesArray.length) {
     //   // hash containing relevant attribute keys on model that ends with `Id` or is 'id'
     //   let attrKeys = pluckKeysMatchingExpression(entriesArray[0], /^(i|(\w)+I)d$/)
@@ -95,23 +93,23 @@ export default class DomainStore {
     //   let hashIds = pickHashOfIdsAsArray(entriesArray, attrKeys)
     //   triggerFetchOtherStore(this, this.entries, hashIds)
     // }
-    return this.entries;
+    return this.entries
   }
 
   _fetchAllFinally() {
-    this.status.isLoading = false;
-    return this.entries;
+    this.status.isLoading = false
+    return this.entries
   }
 
   _fetchOne(params = {}) {
     if (!comparePropertyToType(params.id, types.number, { similar: [types.string] })) {
-      throw new TypeError('Expected "id" as "number" or "string" in params');
+      throw new TypeError('Expected "id" as "number" or "string" in params')
     }
-    return this._api.get(params);
+    return this._api.get(params)
   }
 
   _fetchOneSuccess(data) {
-    let modelData = this._pushEntry(data);
+    let modelData = this._pushEntry(data)
 
     // if (this._relationships.length && Object.values(modelData).length) {
     //   // hash containing relevant attribute keys on model that ends with `Id` or is 'id'
@@ -123,11 +121,11 @@ export default class DomainStore {
     //   triggerFetchOtherStore(this, modelEntry, hashIds)
     // }
 
-    return modelData;
+    return modelData
   }
 
   _fetchOneFinally() {
-    this.status.isLoading = false;
+    this.status.isLoading = false
   }
 
   _createOne(modelJson) {
@@ -135,127 +133,127 @@ export default class DomainStore {
   }
 
   _createOneSuccess(data) {
-    return this._pushEntry(data);
+    return this._pushEntry(data)
   }
 
   _createOneFinally(modelEntry) {
-    modelEntry.status.isSaving = false;
-    modelEntry.status.isNew = false;
+    modelEntry.status.isSaving = false
+    modelEntry.status.isNew = false
   }
 
   _updateOne(modelEntry) {
-    return this._api.put(modelEntry._serialize());
+    return this._api.put(modelEntry._serialize())
   }
 
   _updateOneSuccess(data) {
-    return this._pushEntry(data);
+    return this._pushEntry(data)
   }
 
   _updateOneFinally(modelEntry) {
-    modelEntry.status.isSaving = false;
+    modelEntry.status.isSaving = false
   }
 
   _updateManySuccess(dataArray) {
-    return dataArray.map((data) => this._updateOneSuccess(data));
+    return dataArray.map((data) => this._updateOneSuccess(data))
   }
 
   _deleteOne(modelEntry) {
-    return this._api.delete(modelEntry);
+    return this._api.delete(modelEntry)
   }
 
   _deleteOneFinally(modelEntry) {
-    modelEntry.status.isSaving = false;
-    modelEntry.status.isDeleted = true;
+    modelEntry.status.isSaving = false
+    modelEntry.status.isDeleted = true
   }
 
   //--
 
   //-- always hits cache
   entriesArray() {
-    let allKeys = Object.assign({}, this.entries);
-    delete allKeys.length;
-    return Object.keys(allKeys).map((key) => this.entries[key]);
+    let allKeys = Object.assign({}, this.entries)
+    delete allKeys.length
+    return Object.keys(allKeys).map((key) => this.entries[key])
   }
 
   all(toJson) {
-    return toJson ? this.entriesArray().map((entry) => entry._serialize()) : this.entries;
+    return toJson ? this.entriesArray().map((entry) => entry._serialize()) : this.entries
   }
 
   find(id, toJson) {
     if (!comparePropertyToType(id, types.number, { similar: [types.string] })) {
-      throw new TypeError('Expected "id" as "number" or "string"');
+      throw new TypeError('Expected "id" as "number" or "string"')
     }
-    const entry = this.entries[id];
-    return toJson ? entry._serialize() : entry;
+    const entry = this.entries[id]
+    return toJson ? entry._serialize() : entry
   }
   //--
 
   //-- hits cache before dispatching network request (returns promise)
   findOrShowEntry(id, params, { successCallback, errorCallback, finallyCallback } = {}) {
-    const cache = this.find(id);
+    const cache = this.find(id)
     if (!cache) {
-      return this.showEntry(id, params, { successCallback, errorCallback, finallyCallback });
+      return this.showEntry(id, params, { successCallback, errorCallback, finallyCallback })
     }
-    return Promise.resolve(cache);
+    return Promise.resolve(cache)
   }
 
   allOrListEntries(toJson, params, { successCallback, errorCallback, finallyCallback } = {}) {
-    const cache = this.all(toJson);
+    const cache = this.all(toJson)
     if (!cache.length) {
-      return this.listEntries(params, { successCallback, errorCallback, finallyCallback });
+      return this.listEntries(params, { successCallback, errorCallback, finallyCallback })
     }
-    return Promise.resolve(cache);
+    return Promise.resolve(cache)
   }
   //--
 
   async listEntries(params, { successCallback, errorCallback, finallyCallback } = {}) {
-    successCallback = successCallback || this._fetchAllSuccess.bind(this);
-    errorCallback = errorCallback || this._genericError.bind(this);
-    finallyCallback = finallyCallback || this._fetchAllFinally.bind(this);
+    successCallback = successCallback || this._fetchAllSuccess.bind(this)
+    errorCallback = errorCallback || this._genericError.bind(this)
+    finallyCallback = finallyCallback || this._fetchAllFinally.bind(this)
 
-    this.status.isLoading = true;
-    let data = null;
-    let model = null;
+    this.status.isLoading = true
+    let data = null
+    let model = null
 
     try {
-      data = await this._fetchAll(params);
-      model = successCallback(data);
+      data = await this._fetchAll(params)
+      model = successCallback(data)
     } catch(error) {
-      errorCallback(error);
+      errorCallback(error)
     } finally {
-      finallyCallback();
+      finallyCallback()
     }
-    return model;
+    return model
   }
 
   async showEntry(id, params, { successCallback, errorCallback, finallyCallback } = {}) {
     if (!comparePropertyToType(id, types.number, { similar: [types.string] })) {
-      throw new TypeError('Expected "id" as "number" or "string"');
+      throw new TypeError('Expected "id" as "number" or "string"')
     }
 
-    successCallback = successCallback || this._fetchOneSuccess.bind(this);
-    errorCallback = errorCallback || this._genericError.bind(this);
-    finallyCallback = finallyCallback || this._fetchOneFinally.bind(this);
+    successCallback = successCallback || this._fetchOneSuccess.bind(this)
+    errorCallback = errorCallback || this._genericError.bind(this)
+    finallyCallback = finallyCallback || this._fetchOneFinally.bind(this)
 
-    this.status.isLoading = true;
-    let data = null;
-    let model = null;
+    this.status.isLoading = true
+    let data = null
+    let model = null
     try {
-      data = await this._fetchOne(mergeObject(params, { id }));
-      model = successCallback(data);
+      data = await this._fetchOne(mergeObject(params, { id }))
+      model = successCallback(data)
     } catch(error) {
-      errorCallback(error);
+      errorCallback(error)
     } finally {
-      finallyCallback();
+      finallyCallback()
     }
-    return model;
+    return model
   }
 
   /* *
    *  Creates the model object but doesn't persist it until the model.save() is called
    */
   createRecord(modelJson) {
-    return this._createRecord(modelJson, { isNew: true });
+    return this._createRecord(modelJson, { isNew: true })
   }
 
   /* *
@@ -265,83 +263,83 @@ export default class DomainStore {
     // pre-processing
     let modelJson = null, modelEntry = null
     if (comparePropertyToType(modelEntryJson, types.object)) {
-      modelJson = setObject({}, modelEntryJson, snakecase);
-      modelEntry = this.createRecord(modelJson);
+      modelJson = setObject({}, modelEntryJson, snakecase)
+      modelEntry = this.createRecord(modelJson)
     } else if (modelEntryJson._serialize) {
       modelJson = modelEntryJson._serialize()
     } else {
-      throw new TypeError('Expecting model instance or json object');
+      throw new TypeError('Expecting model instance or json object')
     }
 
-    successCallback = successCallback || this._createOneSuccess.bind(this);
-    errorCallback = errorCallback || this._genericError.bind(this);
-    finallyCallback = finallyCallback || this._createOneFinally.bind(this, modelEntry);
+    successCallback = successCallback || this._createOneSuccess.bind(this)
+    errorCallback = errorCallback || this._genericError.bind(this)
+    finallyCallback = finallyCallback || this._createOneFinally.bind(this, modelEntry)
 
-    modelEntry.status.isSaving = true;
+    modelEntry.status.isSaving = true
 
     try {
-      let response = await this._createOne(modelJson);
-      modelEntry = successCallback(response);
+      let response = await this._createOne(modelJson)
+      modelEntry = successCallback(response)
     } catch(error) {
-      errorCallback(error);
+      errorCallback(error)
     } finally {
-      finallyCallback(modelEntry);
+      finallyCallback(modelEntry)
     }
-    return modelEntry;
+    return modelEntry
   }
 
   async updateEntry(modelEntry, { successCallback, errorCallback, finallyCallback } = {}) {
-    successCallback = successCallback || this._updateOneSuccess.bind(this);
-    errorCallback = errorCallback || this._genericError.bind(this);
-    finallyCallback = finallyCallback || this._updateOneFinally.bind(this);
+    successCallback = successCallback || this._updateOneSuccess.bind(this)
+    errorCallback = errorCallback || this._genericError.bind(this)
+    finallyCallback = finallyCallback || this._updateOneFinally.bind(this)
 
-    modelEntry.status.isSaving = true;
+    modelEntry.status.isSaving = true
 
     try {
-      let response = await this._updateOne(modelEntry);
-      modelEntry = successCallback(response);
+      let response = await this._updateOne(modelEntry)
+      modelEntry = successCallback(response)
     } catch(error) {
-      errorCallback(error);
+      errorCallback(error)
     } finally {
-      finallyCallback(modelEntry);
+      finallyCallback(modelEntry)
     }
-    return modelEntry;
+    return modelEntry
   }
 
   async updateEntries(modelEntriesObjectArray, { successCallback, errorCallback } = {}) {
-    let modelEntries = modelEntriesObjectArray;
+    let modelEntries = modelEntriesObjectArray
     if (comparePropertyToType(modelEntriesObjectArray, types.object)) {
-      modelEntries = Object.keys(modelEntriesObjectArray).map((key) => modelEntriesObjectArray[key]);
+      modelEntries = Object.keys(modelEntriesObjectArray).map((key) => modelEntriesObjectArray[key])
     } else if (!comparePropertyToType(modelEntriesObjectArray, types.array)) {
-      throw new TypeError('Expected "array" or "object" with model entries as object values');
+      throw new TypeError('Expected "array" or "object" with model entries as object values')
     }
 
-    successCallback = successCallback || this._updateManySuccess.bind(this);
-    errorCallback = errorCallback || this._genericError.bind(this);
+    successCallback = successCallback || this._updateManySuccess.bind(this)
+    errorCallback = errorCallback || this._genericError.bind(this)
 
     try {
-      let response = await Promise.all(modelEntries.map((modelEntry) =>  this._updateOne(modelEntry)));
-      modelEntries = successCallback(response);
+      let response = await Promise.all(modelEntries.map((modelEntry) =>  this._updateOne(modelEntry)))
+      modelEntries = successCallback(response)
     } catch(error) {
-      errorCallback(error);
+      errorCallback(error)
     }
-    return modelEntries;
+    return modelEntries
   }
 
-  deleteEntry(modelId, { successCallback, errorCallback, finallyCallback } = {}) {
+  deleteEntry(modelId, { errorCallback, finallyCallback } = {}) {
     if (!comparePropertyToType(modelId, types.number, { similar: [types.string] })) {
-      throw new TypeError('Expected "modelId" of type "number" or "string"');
+      throw new TypeError('Expected "modelId" of type "number" or "string"')
     }
 
-    const modelEntry = this.entries[modelId];
+    const modelEntry = this.entries[modelId]
 
-    errorCallback = errorCallback || this._genericError.bind(this);
-    finallyCallback = finallyCallback || this._deleteOneFinally.bind(this, modelEntry);
+    errorCallback = errorCallback || this._genericError.bind(this)
+    finallyCallback = finallyCallback || this._deleteOneFinally.bind(this, modelEntry)
 
-    modelEntry.status.isSaving = true;
+    modelEntry.status.isSaving = true
     return this._deleteOne(modelEntry)
-      .then((response) => this._deleteEntry(modelEntry.id))
+      .then((_response) => this._deleteEntry(modelEntry.id))
       .catch(errorCallback)
-      .finally(finallyCallback);
+      .finally(finallyCallback)
   }
 }
--- src/fetcher.js
+++ src/fetcher.js
@@ -1,52 +1,52 @@
 import inflection from 'inflection'
 
 const getCrsfToken = function() {
-  let dom = document.getElementsByName('csrf-token');
-  return dom.length ? dom[0].getAttribute('content') : '';
+  let dom = document.getElementsByName('csrf-token')
+  return dom.length ? dom[0].getAttribute('content') : ''
 }
 
 const defaultHeaders = {
   'csrf-token': window.csrfToken || getCrsfToken(),
   'Accept': 'application/json',
   'Content-Type': 'application/json',
-};
+}
 
 const stringifyParams = function(data) {
   return Object.keys(data).map(function(key) {
     return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-  }).join('&');
+  }).join('&')
 }
 
 const fetcher = function(method, url) {
   return (data = {}, headers = {}, credentials = 'same-origin') => {
-    let newUrl = url.slice(0);
+    let newUrl = url.slice(0)
     let options = {
       method: method.toUpperCase(),
       credentials: credentials,
       headers: Object.assign({}, defaultHeaders, headers),
-    };
+    }
 
     if (data.id) {
-      newUrl = `${url}/${data.id}`;
-      delete data.id;
+      newUrl = `${url}/${data.id}`
+      delete data.id
     }
 
     if (method !== 'get') {
-      options.body = JSON.stringify(data);
+      options.body = JSON.stringify(data)
     } else if (Object.keys(data).length) {
-      newUrl = `${newUrl}?${stringifyParams(data)}`;
+      newUrl = `${newUrl}?${stringifyParams(data)}`
     }
 
-    return window.fetch(newUrl, options).then(res => res.ok ? res.json() : Promise.reject(res));
+    return window.fetch(newUrl, options).then(res => res.ok ? res.json() : Promise.reject(res))
   }
-};
+}
 
 const generateApi = function(basePath, modelName) {
   return ['get', 'post', 'put', 'delete'].reduce((api, method) => {
-    api[method] = fetcher(method, `${basePath}/${inflection.pluralize(modelName)}`);
-    return api;
-  }, {});
-};
+    api[method] = fetcher(method, `${basePath}/${inflection.pluralize(modelName)}`)
+    return api
+  }, {})
+}
 
 export {
   fetcher,
