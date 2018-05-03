73755ac61bac00d7d0daba4949176529cfe9770c
ngyv <ngyv.codes@gmail.com>
2018-05-03 09:13:47 +0800

    [LINT] Remove semicolon and unused variables

--- test/base-model-test.js
+++ test/base-model-test.js
@@ -1,13 +1,13 @@
-import test from 'ava';
-import { types } from '@ngyv/prop-utils';
-import reModelr from '../lib';
+import test from 'ava'
+import { types } from '@ngyv/prop-utils'
+import reModelr from '../lib'
 
 test.beforeEach(t => {
-  const { BaseModel } = reModelr;
+  const { BaseModel } = reModelr
 
   class User extends BaseModel {
     _attributes() {
-      const defaultAttributes = super._attributes();
+      const defaultAttributes = super._attributes()
       const userAttributes = {
         name: {
           type: types.string,
@@ -15,42 +15,42 @@ test.beforeEach(t => {
           ignoreTypes: [types.undefined, types.null, types.emptyString],
         },
         favouriteFood: types.array,
-      };
-      return Object.assign({}, defaultAttributes, userAttributes);
+      }
+      return Object.assign({}, defaultAttributes, userAttributes)
     }
   }
   class UserStore {}
-  const userStore = new UserStore();
+  const userStore = new UserStore()
 
   let userModel = new User(userStore, {
     id: 1,
     name: 'Avo',
     createdAt: new Date(),
     updatedAt: new Date(),
-  });
+  })
 
-  t.context.UserClass = User;
-  t.context.userStore = userStore;
-  t.context.userModel = userModel;
-});
+  t.context.UserClass = User
+  t.context.userStore = userStore
+  t.context.userModel = userModel
+})
 
 test('Base model | requires domain store as class instance', t => {
   const error = t.throws(() => {
     new t.context.UserClass({
       name: 'Tan'
-    });
-  }, TypeError);
-  t.is(error.message, 'Invalid domain store');
-});
+    })
+  }, TypeError)
+  t.is(error.message, 'Invalid domain store')
+})
 
 test('Base model | validates name', t => {
   const error = t.throws(() => {
     new t.context.UserClass(t.context.userStore, {
       name: 10
-    });
-  }, TypeError);
-  t.is(error.message, 'Expected "string" but got "number" instead for "name"');
-});
+    })
+  }, TypeError)
+  t.is(error.message, 'Expected "string" but got "number" instead for "name"')
+})
 
 test('Base model | creates user', t => {
   let anotherUser = new t.context.UserClass(t.context.userStore, {
@@ -60,74 +60,74 @@ test('Base model | creates user', t => {
   },
   {
     isNew: true,
-  });
+  })
 
-  t.deepEqual(anotherUser.get('favouriteFood'), ['poke', 'pho'], 'Deserializes json with camelcase keys on instance');
-  t.deepEqual(anotherUser.get('_data'), { id: 2, name: 'Cado', favouriteFood:  ['poke', 'pho'] }, 'Caches with camelcase key on "_data"');
-  t.deepEqual(anotherUser.get('status'), { isSaving: false, isNew: true, isDeleted: false }, 'Sets status with default states and params passed');
-});
+  t.deepEqual(anotherUser.get('favouriteFood'), ['poke', 'pho'], 'Deserializes json with camelcase keys on instance')
+  t.deepEqual(anotherUser.get('_data'), { id: 2, name: 'Cado', favouriteFood:  ['poke', 'pho'] }, 'Caches with camelcase key on "_data"')
+  t.deepEqual(anotherUser.get('status'), { isSaving: false, isNew: true, isDeleted: false }, 'Sets status with default states and params passed')
+})
 
 test('Base model | modifies user attributes reflect in changed attributes and is dirty state', t => {
-  let user = t.context.userModel;
-  t.is(user.get('name'), 'Avo', 'Initial user');
-  t.deepEqual(user.changedAttributes(), {}, 'Initial changed attributes is an empty hash');
-  t.is(user.isDirty(), false, 'Initial dirty state is false');
+  let user = t.context.userModel
+  t.is(user.get('name'), 'Avo', 'Initial user')
+  t.deepEqual(user.changedAttributes(), {}, 'Initial changed attributes is an empty hash')
+  t.is(user.isDirty(), false, 'Initial dirty state is false')
 
-  user.set('name', 'Cado');
-  t.is(user.get('name'), 'Cado', 'User model name is modified');
-  t.deepEqual(user.changedAttributes(), { name: ['Avo', 'Cado'] }, 'Changed name is reflected with before and after values');
-  t.is(user.isDirty(), true, 'Dirty state is now true');
-});
+  user.set('name', 'Cado')
+  t.is(user.get('name'), 'Cado', 'User model name is modified')
+  t.deepEqual(user.changedAttributes(), { name: ['Avo', 'Cado'] }, 'Changed name is reflected with before and after values')
+  t.is(user.isDirty(), true, 'Dirty state is now true')
+})
 
 test('Base model | modifies user attributes and discarding changes successfully', t => {
-  let user = t.context.userModel;
-  t.is(user.get('name'), 'Avo', 'Initial user');
+  let user = t.context.userModel
+  t.is(user.get('name'), 'Avo', 'Initial user')
 
-  user.set('name', 'Cado');
-  t.is(user.get('name'), 'Cado', 'User model name is modified');
-  t.is(user.isDirty(), true, 'Dirty state is true');
+  user.set('name', 'Cado')
+  t.is(user.get('name'), 'Cado', 'User model name is modified')
+  t.is(user.isDirty(), true, 'Dirty state is true')
 
-  user.discardChanges();
-  t.is(user.get('name'), 'Avo', 'User model name is restored');
-  t.is(user.isDirty(), false, 'Dirty state is now false');
-});
+  user.discardChanges()
+  t.is(user.get('name'), 'Avo', 'User model name is restored')
+  t.is(user.isDirty(), false, 'Dirty state is now false')
+})
 
 test('Base model | soft deleting user marks status and changes to is dirty', t => {
-  let user = t.context.userModel;
-  t.is(user.get('status.isDeleted'), false, 'Initial user `status.isDeleted` is false');
-  t.is(user.isDirty(), false, 'Initial user is not dirty'); // :smirk:
+  let user = t.context.userModel
+  t.is(user.get('status.isDeleted'), false, 'Initial user `status.isDeleted` is false')
+  t.is(user.isDirty(), false, 'Initial user is not dirty') // :smirk:
 
-  user.softDelete();
-  t.is(user.get('status.isDeleted'), true, 'User `status.isDeleted` is now true');
-  t.is(user.isDirty(), true, 'Initial user is now dirty'); // :smiling_imp:
-});
+  user.softDelete()
+  t.is(user.get('status.isDeleted'), true, 'User `status.isDeleted` is now true')
+  t.is(user.isDirty(), true, 'Initial user is now dirty') // :smiling_imp:
+})
 
 test('Base model | saves itself via store', t => {
-  let { userStore } = t.context;
-  t.plan(3);
+  let { userStore } = t.context
+  t.plan(3)
 
   let newUser = new t.context.UserClass(userStore, {
     id: 1,
     name: 'Tan Hong Ming',
     likes: 'Ummi Khazriena'
-  }, { isNew: true });
+  }, { isNew: true })
 
   // Stub the methods. Domain store methods tested separately.
-  userStore.createEntry = (model) => t.is(model.status.isNew, true);
-  newUser.save();
+  userStore.createEntry = (model) => t.is(model.status.isNew, true)
+  newUser.save()
 
-  newUser.set('likes', undefined);
-  userStore.updateEntry = (model) => t.is(model.likes, undefined);
-  newUser.save();
+  newUser.set('likes', undefined)
+  userStore.updateEntry = (model) => t.is(model.likes, undefined)
+  newUser.save()
 
-  newUser.softDelete();
-  userStore.deleteEntry = (model) => t.is(model.status.isDeleted, true);
-  newUser.save();
-});
+  newUser.softDelete()
+  userStore.deleteEntry = (model) => t.is(model.status.isDeleted, true)
+  newUser.save()
+})
 
 test('Base model | deletes itself via store', t => {
-  let { userStore, userModel } = t.context;
+  let { userStore, userModel } = t.context
 
-  userStore.deleteEntry = (model) => t.pass();
-  userModel.delete();
-});
+  userStore.deleteEntry = (_model) => t.pass()
+  userModel.delete()
+})
--- test/domain-store-test.js
+++ test/domain-store-test.js
@@ -1,60 +1,60 @@
-import test from 'ava';
-import http from 'ava-http';
-import { types, identify } from '@ngyv/prop-utils';
-import reModelr from '../lib';
+import test from 'ava'
+import http from 'ava-http'
+import { types } from '@ngyv/prop-utils'
+import reModelr from '../lib'
 
 test.beforeEach(t => {
-  const { BaseModel, DomainStore } = reModelr;
+  const { BaseModel, DomainStore } = reModelr
 
   class User extends BaseModel {
     _attributes() {
-      const defaultAttributes = super._attributes();
+      const defaultAttributes = super._attributes()
       const userAttributes = {
         name: types.string,
         age: types.number,
         favouriteFood: types.array,
-      };
-      return Object.assign({}, defaultAttributes, userAttributes);
+      }
+      return Object.assign({}, defaultAttributes, userAttributes)
     }
   }
-  const mockBasePath = 'http://localhost:3000/api';
-  const mockApiPath = `${mockBasePath}/users`;
+  const mockBasePath = 'http://localhost:3000/api'
+  const mockApiPath = `${mockBasePath}/users`
   class UserStore extends DomainStore {
     constructor(){
       super(User, { basePath: mockBasePath })
     }
   }
-  const userStore = new UserStore();
+  const userStore = new UserStore()
 
-  t.context.UserClass = User;
-  t.context.userStore = userStore;
+  t.context.UserClass = User
+  t.context.userStore = userStore
 
   // Stub the api instead of using fetcher
   t.context.userStore._api = {
     get: function(params) {
       if (params.id) {
-        const id = params.id;
-        delete params.id;
-        return http.get(`${mockApiPath}/${id}`, { params });
+        const id = params.id
+        delete params.id
+        return http.get(`${mockApiPath}/${id}`, { params })
       }
-      return http.get(mockApiPath, { params });
+      return http.get(mockApiPath, { params })
     },
     post: function(params) {
-      return http.post(mockApiPath, { params });
+      return http.post(mockApiPath, { params })
     },
     put: function(params) {
-      return http.put(`${mockApiPath}/${params.id}`, { params });
+      return http.put(`${mockApiPath}/${params.id}`, { params })
     },
     delete: function(params) {
-      return http.del(`${mockApiPath}/${params.id}`, { params });
+      return http.del(`${mockApiPath}/${params.id}`, { params })
     },
-  };
-});
+  }
+})
 
 test('Domain Store | listEntries', async t => {
-  const entries = await t.context.userStore.listEntries();
-  t.is(entries.length, 3, 'Fetched 3 users');
-  t.is(entries[1].constructor.name, 'User', 'Uses model class to instantiate response');
+  const entries = await t.context.userStore.listEntries()
+  t.is(entries.length, 3, 'Fetched 3 users')
+  t.is(entries[1].constructor.name, 'User', 'Uses model class to instantiate response')
   t.deepEqual(entries[1]._data, {
     age: '31',
     createdAt: '2018-02-11T10:34:22.032Z',
@@ -62,11 +62,11 @@ test('Domain Store | listEntries', async t => {
     id: 1,
     name: 'Siti Nurhaliza',
     updatedAt: '2018-03-24T08:56:47.100Z',
-  }, 'Caches on _data');
-});
+  }, 'Caches on _data')
+})
 
 test('Domain Store | showEntry', async t => {
-  const entry = await t.context.userStore.showEntry(1);
+  const entry = await t.context.userStore.showEntry(1)
   t.deepEqual(entry._data, {
     age: '31',
     createdAt: '2018-02-11T10:34:22.032Z',
@@ -74,18 +74,18 @@ test('Domain Store | showEntry', async t => {
     id: 1,
     name: 'Siti Nurhaliza',
     updatedAt: '2018-03-24T08:56:47.100Z',
-  });
-});
+  })
+})
 
 test('Domain store | createEntry', async t => {
-  const { userStore } = t.context;
+  const { userStore } = t.context
   const modelJson = {
     name: 'Elizabeth Tan',
     age: 24,
     favouriteFood: ['bak kut teh', 'har mee']
-  };
+  }
 
-  const user = await userStore.createEntry(modelJson);
+  const user = await userStore.createEntry(modelJson)
 
   t.deepEqual(user._data, {
     age: modelJson.age.toString(),
@@ -94,100 +94,100 @@ test('Domain store | createEntry', async t => {
     id: 4,
     name: modelJson.name,
     updatedAt: '2018-04-02T09:12:20.221Z',
-  });
-});
+  })
+})
 
 test('Domain store | updateEntry', async t => {
-  const { userStore } = t.context;
-  await userStore.listEntries();
+  const { userStore } = t.context
+  await userStore.listEntries()
 
-  let modelEntry =  userStore.entries[1];
-  t.is(modelEntry.get('age'), '31', 'Initial age is 31');
-  modelEntry.set('age', 21);
+  let modelEntry =  userStore.entries[1]
+  t.is(modelEntry.get('age'), '31', 'Initial age is 31')
+  modelEntry.set('age', 21)
 
-  const entry = await userStore.updateEntry(modelEntry);
-  t.is(entry._data.age, '21', 'Serializes model with updated age');
-});
+  const entry = await userStore.updateEntry(modelEntry)
+  t.is(entry._data.age, '21', 'Serializes model with updated age')
+})
 
 test('Domain store | updateEntries', async t => {
-  const { userStore } = t.context;
+  const { userStore } = t.context
 
-  await userStore.listEntries();
+  await userStore.listEntries()
 
-  const entryOneData = Object.assign({}, userStore.entries[1]._data);
-  const entryTwoData = Object.assign({}, userStore.entries[2]._data);
+  const entryOneData = Object.assign({}, userStore.entries[1]._data)
+  const entryTwoData = Object.assign({}, userStore.entries[2]._data)
 
-  const error = await t.throws(userStore.updateEntries(1), TypeError);
-  t.is(error.message, 'Expected "array" or "object" with model entries as object values');
+  const error = await t.throws(userStore.updateEntries(1), TypeError)
+  t.is(error.message, 'Expected "array" or "object" with model entries as object values')
   t.truthy(userStore.updateEntries({1: userStore.entries[1], 2: userStore.entries[2]}))
 
-  let modelArray = [userStore.entries[1], userStore.entries[2]];
-  const newAge = 21;
-  const newName = 'Rando';
-  modelArray[0].set('age', newAge);
-  modelArray[1].set('name', newName);
+  let modelArray = [userStore.entries[1], userStore.entries[2]]
+  const newAge = 21
+  const newName = 'Rando'
+  modelArray[0].set('age', newAge)
+  modelArray[1].set('name', newName)
 
-  let response = await userStore.updateEntries(modelArray);
+  await userStore.updateEntries(modelArray)
 
-  t.deepEqual(userStore.entries[1]._data, Object.assign({}, entryOneData, { age: newAge.toString() }));
-  t.deepEqual(userStore.entries[2]._data, Object.assign({}, entryTwoData, { name: newName }));
-});
+  t.deepEqual(userStore.entries[1]._data, Object.assign({}, entryOneData, { age: newAge.toString() }))
+  t.deepEqual(userStore.entries[2]._data, Object.assign({}, entryTwoData, { name: newName }))
+})
 
 test('Domain store | deleteEntry', async t => {
-  const { userStore } = t.context;
-  await userStore.listEntries();
+  const { userStore } = t.context
+  await userStore.listEntries()
 
-  const deleteId = 1;
+  const deleteId = 1
   const error = await t.throws(() => {
-    userStore.deleteEntry(userStore.entries[deleteId]);
-  }, TypeError);
-  t.is(error.message, 'Expected "modelId" of type "number" or "string"');
+    userStore.deleteEntry(userStore.entries[deleteId])
+  }, TypeError)
+  t.is(error.message, 'Expected "modelId" of type "number" or "string"')
 
-  await userStore.deleteEntry(deleteId);
-  t.deepEqual(userStore.entries[deleteId], undefined);
-});
+  await userStore.deleteEntry(deleteId)
+  t.deepEqual(userStore.entries[deleteId], undefined)
+})
 
 //== Other helper methods
 test('Domain Store | entriesArray', async t => {
-  const { userStore } = t.context;
+  const { userStore } = t.context
 
-  t.deepEqual(userStore.entriesArray, [], 'Entries array is empty');
-  await userStore.listEntries();
-  t.deepEqual(userStore.entriesArray, [userStore.entries[1], userStore.entries[2], userStore.entries[3]], 'Entries array contains model entries');
-});
+  t.deepEqual(userStore.entriesArray, [], 'Entries array is empty')
+  await userStore.listEntries()
+  t.deepEqual(userStore.entriesArray, [userStore.entries[1], userStore.entries[2], userStore.entries[3]], 'Entries array contains model entries')
+})
 
 test('Domain Store | all', async t => {
-  const { userStore } = t.context;
+  const { userStore } = t.context
 
-  t.deepEqual(userStore.entries, {}, 'Store is empty');
-  t.deepEqual(userStore.all(), {}, 'Method always hits cached entries');
+  t.deepEqual(userStore.entries, {}, 'Store is empty')
+  t.deepEqual(userStore.all(), {}, 'Method always hits cached entries')
 
-  await userStore.listEntries();
-  t.deepEqual(userStore.all(), userStore.entries, 'Returns model entries');
-  const toJson = true;
+  await userStore.listEntries()
+  t.deepEqual(userStore.all(), userStore.entries, 'Returns model entries')
+  const toJson = true
   t.deepEqual(userStore.all(toJson), [
     userStore.entries[1]._serialize(),
     userStore.entries[2]._serialize(),
     userStore.entries[3]._serialize(),
-  ], 'Returns array of serialized model entries where key is snakecase');
-});
+  ], 'Returns array of serialized model entries where key is snakecase')
+})
 
 test('Domain Store | find', async t => {
-  const { userStore } = t.context;
-  const modelId = 1;
+  const { userStore } = t.context
+  const modelId = 1
 
   const error = await t.throws(() => {
-    userStore.find();
-  }, TypeError);
-  t.is(error.message, 'Expected "id" as "number" or "string"');
+    userStore.find()
+  }, TypeError)
+  t.is(error.message, 'Expected "id" as "number" or "string"')
 
 
-  t.deepEqual(userStore.entries, {}, 'Store is empty');
-  t.deepEqual(userStore.find(modelId), undefined, 'Method always hits cached entry');
+  t.deepEqual(userStore.entries, {}, 'Store is empty')
+  t.deepEqual(userStore.find(modelId), undefined, 'Method always hits cached entry')
 
-  await userStore.showEntry(1);
-  t.deepEqual(userStore.find(modelId), userStore.entries[modelId], 'Returns model entry');
-  const toJson = true;
+  await userStore.showEntry(1)
+  t.deepEqual(userStore.find(modelId), userStore.entries[modelId], 'Returns model entry')
+  const toJson = true
   t.deepEqual(userStore.find(modelId, toJson), {
     age: '31',
     created_at: '2018-02-11T10:34:22.032Z',
@@ -195,48 +195,48 @@ test('Domain Store | find', async t => {
     id: 1,
     name: 'Siti Nurhaliza',
     updated_at: '2018-03-24T08:56:47.100Z',
-  }, 'Returns serialized model entry');
-});
+  }, 'Returns serialized model entry')
+})
 
 test('Domain Store | findOrShowEntry', async t => {
-  const { userStore } = t.context;
-  const modelId = 1;
-  t.plan(4);
+  const { userStore } = t.context
+  const modelId = 1
+  t.plan(4)
 
-  t.is(userStore.entries[modelId], undefined, 'Entry is not found in store cache');
-  const response = userStore.findOrShowEntry(1);
-  t.is(response.constructor.name, 'Promise', 'Makes a request');
-  await response;
-  t.not(userStore.entries[modelId], undefined, 'Entry is now found in store cache');
-  userStore._api.get = () => t.fail('Should not make another network request');
-  t.is(userStore.findOrShowEntry(1).constructor.name, 'Promise', 'Always returns as a promise');
-});
+  t.is(userStore.entries[modelId], undefined, 'Entry is not found in store cache')
+  const response = userStore.findOrShowEntry(1)
+  t.is(response.constructor.name, 'Promise', 'Makes a request')
+  await response
+  t.not(userStore.entries[modelId], undefined, 'Entry is now found in store cache')
+  userStore._api.get = () => t.fail('Should not make another network request')
+  t.is(userStore.findOrShowEntry(1).constructor.name, 'Promise', 'Always returns as a promise')
+})
 
 test('Domain Store | allOrListEntries', async t => {
-  const { userStore } = t.context;
-  t.plan(5);
+  const { userStore } = t.context
+  t.plan(5)
 
-  t.deepEqual(userStore.entries, {}, 'No entries found in store cache');
-  const params = { filter: 'random' };
-  const callbacks = { successCallback: () => 'successCallback' , errorCallback: () => 'errorCallback', finallyCallback: () => 'finallyCallback' };
+  t.deepEqual(userStore.entries, {}, 'No entries found in store cache')
+  const params = { filter: 'random' }
+  const callbacks = { successCallback: () => 'successCallback' , errorCallback: () => 'errorCallback', finallyCallback: () => 'finallyCallback' }
 
   userStore.listEntries = (...args) => {
-    t.pass('Tries to list entries from server');
-    t.deepEqual(args[0], params, 'Params are being passed on to "listEntries"');
-    t.deepEqual(args[1], callbacks, 'Callback functions are being passed on to "listEntries"');
-  };
-  const toJson = false;
-  await userStore.allOrListEntries(toJson, params, callbacks);
-  userStore.entries = { 1: { id: 1 }, length: 1 }; // stub
-  userStore.listEntries = () => t.fail('Should not make another network request');
-  t.is(userStore.allOrListEntries().constructor.name, 'Promise', 'Always returns as a promise');
-});
+    t.pass('Tries to list entries from server')
+    t.deepEqual(args[0], params, 'Params are being passed on to "listEntries"')
+    t.deepEqual(args[1], callbacks, 'Callback functions are being passed on to "listEntries"')
+  }
+  const toJson = false
+  await userStore.allOrListEntries(toJson, params, callbacks)
+  userStore.entries = { 1: { id: 1 }, length: 1 } // stub
+  userStore.listEntries = () => t.fail('Should not make another network request')
+  t.is(userStore.allOrListEntries().constructor.name, 'Promise', 'Always returns as a promise')
+})
 
 test('Domain Store | createRecord', t => {
-  const { userStore } = t.context;
+  const { userStore } = t.context
 
-  const record = userStore.createRecord({ id: 1 });
-  t.is(record.constructor.name, 'User');
-  t.deepEqual(record._data, { id: 1 });
-  t.deepEqual(record.status, { isDeleted: false, isNew: true, isSaving: false }, 'Creates record with isNew status flagged');
-});
+  const record = userStore.createRecord({ id: 1 })
+  t.is(record.constructor.name, 'User')
+  t.deepEqual(record._data, { id: 1 })
+  t.deepEqual(record.status, { isDeleted: false, isNew: true, isSaving: false }, 'Creates record with isNew status flagged')
+})
