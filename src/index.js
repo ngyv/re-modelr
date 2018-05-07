c9168e729885048dd04d049d6da46eb26deebbfc
ngyv <ngyv.codes@gmail.com>
2018-05-07 23:33:36 +0800

    [FEATURE] Model descriptor validate function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { type } from './model-descriptors'
+import {
+  type,
+  validate
+} from './model-descriptors'
 import BaseModel from './base-model'
 import DomainStore from './domain-store'
 
 export {
   type,
+  validate,
   BaseModel,
   DomainStore
 }
