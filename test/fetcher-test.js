3c334e4b733eb78f183251eeedd95c2192d5251e
ngyv <ngyv.codes@gmail.com>
2018-05-07 21:38:31 +0800

    [LINT] Semicolons

--- test/fetcher-test.js
+++ test/fetcher-test.js
@@ -1,8 +1,8 @@
-import test from 'ava';
-import { fetcher, generateApi } from '../lib/fetcher';
+import test from 'ava'
+import { fetcher, generateApi } from '../lib/fetcher'
 
-test.before(t => {
-  window.crsfToken = 'token';
+test.before(_t => {
+  window.crsfToken = 'token'
   window.fetch = (url, { method, body, credentials, headers } = {}) => {
     return Promise.resolve({
       ok: true,
@@ -15,39 +15,39 @@ test.before(t => {
           headers
         }
       }
-    });
-  };
-});
+    })
+  }
+})
 
 test('Fetcher | creates wrapper function to fetch', async t => {
-  const url = '/api/users';
-  const apiGet = fetcher('get', url);
-  let apiGetFunction = null;
-  const data = { id: 1, name: 'Ava' };
-  const showUrl = '/api/users/1?name=Ava';
-  const headers = { 'Accept': 'application/json' };
-  apiGetFunction = await apiGet(data, headers);
+  const url = '/api/users'
+  const apiGet = fetcher('get', url)
+  let apiGetFunction = null
+  const data = { id: 1, name: 'Ava' }
+  const showUrl = '/api/users/1?name=Ava'
+  const headers = { 'Accept': 'application/json' }
+  apiGetFunction = await apiGet(data, headers)
 
-  t.plan(6);
-  t.is(apiGetFunction.url, showUrl, 'data is passed to fetch as part of url');
+  t.plan(6)
+  t.is(apiGetFunction.url, showUrl, 'data is passed to fetch as part of url')
   t.deepEqual(apiGetFunction.headers, {
-     Accept: 'application/json',
-     'Content-Type': 'application/json',
-     'csrf-token': '', // TODO: fix window.crsfToken is not present when file ran
-   }, 'headers is combined');
-  t.is(apiGetFunction.credentials, 'same-origin', 'default credentials is same-origin');
-  t.is(apiGetFunction.method, 'GET', 'method is uppercased');
-  t.deepEqual(apiGetFunction.body, undefined, 'body is not passed');
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'csrf-token': '', // TODO: fix window.crsfToken is not present when file ran
+  }, 'headers is combined')
+  t.is(apiGetFunction.credentials, 'same-origin', 'default credentials is same-origin')
+  t.is(apiGetFunction.method, 'GET', 'method is uppercased')
+  t.deepEqual(apiGetFunction.body, undefined, 'body is not passed')
 
-  const apiGetFunction2 = await apiGet();
-  t.is(apiGetFunction2.url, url, 'url is not compromised');
-});
+  const apiGetFunction2 = await apiGet()
+  t.is(apiGetFunction2.url, url, 'url is not compromised')
+})
 
 test('Fetcher | generate api with default crud functions', t => {
-  const api = generateApi('/api', 'user');
-  t.plan(4);
-  t.is(typeof api.get, 'function', 'Has "get" function');
-  t.is(typeof api.post, 'function', 'Has "post" function');
-  t.is(typeof api.put, 'function', 'Has "put" function');
-  t.is(typeof api.delete, 'function', 'Has "delete" function');
-});
+  const api = generateApi('/api', 'user')
+  t.plan(4)
+  t.is(typeof api.get, 'function', 'Has "get" function')
+  t.is(typeof api.post, 'function', 'Has "post" function')
+  t.is(typeof api.put, 'function', 'Has "put" function')
+  t.is(typeof api.delete, 'function', 'Has "delete" function')
+})
